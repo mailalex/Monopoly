5ae49e5f01b58cc42aea9820fa95cf320056faaa
mailalex <alexis.maillard@etu.upmf-grenoble.fr>
2015-06-12 15:29:13 +0200

    Merge origin/Tim into Test
    Conflicts:
    	src/Jeu/CarreauPropriete.java
    	src/Jeu/Compagnie.java
    	src/Jeu/Gare.java
    	src/Jeu/Joueur.java
    	src/Jeu/Monopoly.java

--- src/Jeu/CarreauTirage.java
+++ src/Jeu/CarreauTirage.java
@@ -5,6 +5,7 @@ public class CarreauTirage extends CarreauAction {
     public CarreauTirage(int numero, String nomCarreau, Monopoly monopoly) {
         super(numero, nomCarreau, monopoly);
     }
+    
     @Override
     public void action(Joueur j) {
         if (this.getNomCarreau().equals("Chance")) {
